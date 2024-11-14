b499d17661b9e8e8223eab8334b36fe5714fa3a1
Ala123Sobhan <ala123sobhan@gmail.com>
2024-11-14 14:47:38 -0600

    add selenium

--- Selenium.py
+++ Selenium.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
 
 options = Options()
 # options.add_argument('--headless')  # Enable headless mode
@@ -13,5 +14,16 @@ driver = webdriver.Firefox(options=options)  # Or with service: driver = webdriv
 driver.get("https://weathershopper.pythonanywhere.com/")
 title = driver.title
 print(f"Page title: {title}")
+
+temperature = int(driver.find_element(By.ID,"temperature").text.split(" ")[0].strip())
+
+print(temperature)
+
+if temperature < 19:
+    driver.find_element(By.CSS_SELECTOR, "a[href='/moisturizer']").click()
+elif temperature > 34:
+    driver.find_element(By.CSS_SELECTOR, "a[href='/sunscreen']").click()
+
+
 
 driver.quit()
