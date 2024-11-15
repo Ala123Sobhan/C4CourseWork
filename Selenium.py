039bbab67138bcc7cfe5f04e4c0b3157cae92e9c
Ala123Sobhan <ala123sobhan@gmail.com>
2024-11-14 21:27:03 -0600

    add checkout page validation

--- Selenium.py
+++ Selenium.py
@@ -13,17 +13,42 @@ driver = webdriver.Firefox(options=options)  # Or with service: driver = webdriv
 
 driver.get("https://weathershopper.pythonanywhere.com/")
 title = driver.title
+low_temp = False
 print(f"Page title: {title}")
 
 temperature = int(driver.find_element(By.ID,"temperature").text.split(" ")[0].strip())
 
 print(temperature)
 
+
 if temperature < 19:
     driver.find_element(By.CSS_SELECTOR, "a[href='/moisturizer']").click()
+    low_temp = True
 elif temperature > 34:
     driver.find_element(By.CSS_SELECTOR, "a[href='/sunscreen']").click()
 
 
+print(low_temp)
 
-driver.quit()
+if low_temp == True:
+    product_price = driver.find_element(By.CSS_SELECTOR, "div.text-center.text-center.col-4:nth-child(1) p:nth-child(3)").text.split(" ")[2].strip()
+    driver.find_element(By.CSS_SELECTOR, "div.text-center.col-4:nth-child(1) button").click()
+   
+
+else:
+    product_price = driver.find_element(By.CSS_SELECTOR, "div.text-center.text-center.col-4:nth-child(1) p:nth-child(3)").text.split(" ")[2].strip()
+    driver.find_element(By.CSS_SELECTOR, "div.text-center.col-4:nth-child(1) button").click()
+
+print(product_price)
+driver.find_element(By.CSS_SELECTOR, ".thin-text.nav-link").click()
+checkout_price= driver.find_element(By.CSS_SELECTOR, "tbody tr td:nth-child(2)").text.strip()
+assert checkout_price == product_price, f"Expected price {product_price}, but got {checkout_price}"
+
+
+
+
+
+
+
+    
+# driver.quit()
